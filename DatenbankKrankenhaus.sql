b75a7b82d77b729b420746019ad1b3710231c68d
Thomas Konnemann <kick.butts@gmx.de>
2020-05-28 12:23:09 +0200

    Datenbank vergrößert

--- DatenbankKrankenhaus.sql
+++ DatenbankKrankenhaus.sql
@@ -20,9 +20,10 @@ CREATE TABLE arzt
 --
 
 INSERT INTO arzt (name) VALUES
-('Doctor Spock'),
-('Doctor McCoy'),
-('Doctor Who');
+('Franz Meier'),
+('Iris Mueller'),
+('Heike Bachem'),
+('Norbert Kurz');
 
 --
 -- Tabellenstruktur für Tabelle `patienten`
@@ -32,8 +33,8 @@ CREATE TABLE patienten
   patientenID INTEGER AUTO_INCREMENT NOT NULL,
   name VARCHAR(50) NOT NULL,
   vorname VARCHAR(50) NOT NULL,
-  Geschlecht VARCHAR(10),
-  Geburtsjahr INTEGER,
+  geschlecht VARCHAR(10),
+  geburtsjahr YEAR,
   adresse VARCHAR(50)
   PRIMARY KEY (patientenID)
 );
@@ -41,30 +42,72 @@ CREATE TABLE patienten
 --
 -- Daten für Tabelle `patienten`
 --
-INSERT INTO patienten (name, vorname, krankenkasse, behandelnderArzt) VALUES
-  ('vorname VARCHAR(50) NOT NULL', 'Peter', 'AOK', '1'),
-  ('Schmidt', 'Olaf', 'DFG', '2'),
-  ('Peters', 'Otto', 'QWE', '3');
+INSERT INTO patienten (vorname, name, geschlecht, geburtsjahr, adresse) VALUES
+  ('Arnd', 'Ziegel', 'maennlich', 1982, 'Bergstr. 2, 53113 Bonn'),
+  ('Beate', 'Weiler', 'weiblich', 1966, 'Ahrweg 13, 53721 Siegburg'),
+  ('Claus', 'Barth', 'maennlich', 1954, 'Am Breiberg 7, 53229 Bonn'),
+  ('Dieter', 'Bligg', 'maennlich', 1960, 'Rheindamm 15, 53639 Koenigswinter');
 
 
 --
--- Tabellenstruktur für Tabelle `behandelt`
+-- Tabellenstruktur Medikament aufbauen
 --
-/*CREATE TABLE behandelt
+CREATE TABLE medikament
 (
-  patient INTEGER,
-  arzt INTEGER,
-  FOREIGN KEY (patient)
-  REFERENCES patienten(patientenID),
-  FOREIGN KEY (arzt)
-  REFERENCES arzt(arztID)
+  medikamentID INTEGER AUTO_INCREMENT NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  PRIMARY KEY (medikamentID)
+);
+
+--
+-- Daten für Tabelle `medikament`
+--
+INSERT INTO medikament (name) VALUES
+  ('Fluximed'),
+  ('Otoveral'),
+  ('Glyxomycin'),
+  ('Syromoxin');
+
+  --
+  -- Tabellenstruktur für Tabelle `untersuchung`
+  --
+  CREATE TABLE untersuchung
+  (
+    untersuchungID INTEGER AUTO_INCREMENT NOT NULL,
+    arztID INTEGER,
+    patiendID INTEGER,
+    datum DATE,
+    FOREIGN KEY (patiendID)
+    REFERENCES patienten(patientenID),
+    FOREIGN KEY (arztID)
+    REFERENCES arzt(arztID)
+  );
+
+  INSERT INTO untersuchung (arztID, patiendID, datum) VALUES
+    (3,4,'2012-10-03'),
+    (3,3,'2011-21-10'),
+    (2,1,'2012-07-03'),
+    (1,2,'2011-21-10');
+
+
+--
+-- Tabellenstruktur für Tabelle `verordnung`
+--
+CREATE TABLE verordnung
+(
+  untersuchungID INTEGER,
+  medikamentID INTEGER,
+  FOREIGN KEY (untersuchungID)
+  REFERENCES untersuchung(untersuchungID),
+  FOREIGN KEY (medikamentID)
+  REFERENCES medikament(medikamentID)
 );
 
 --
 -- Daten für Tabelle `behandelt`
 --
-INSERT INTO behandelt (patient,arzt) VALUES
-    (1, 1),
-    (2, 2),
-   (3, 3);
-*/
+INSERT INTO verordnung (untersuchungID,medikamentID) VALUES
+    (1, 4),
+    (2, 4),
+    (3, 4),
+   (4, 1);
